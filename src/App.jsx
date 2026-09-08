73b203b82f8e875e3103eaf96880a2cdb55f42f1
Hagar <hagar.khaled.niazi@gmail.com>
2026-09-08 13:00:33 +0300

    Remove Team references from App

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Team from "./pages/Team";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -20,7 +18,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/team" element={<Team />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
