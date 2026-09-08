97f5936fbb23ff454e678bb36d8f3e7ba62a12d2
Hagar <hagar.khaled.niazi@gmail.com>
2026-09-08 12:43:38 +0300

    Revert incorrect data hooks merge

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
-  preview: {
-    port: 4173,
-    host: '0.0.0.0',
-  },
-});
+})
