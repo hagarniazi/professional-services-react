cfb48dada6e677828049d8f30b8016360405c492
Hagar <hagar.khaled.niazi@gmail.com>
2026-09-14 16:42:42 +0300

    Restore project structure after PR #28

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,6 @@
 import "./About.css";
 
 import SectionWrapper from "../components/SectionWrapper";
-import team from "../data/team.json";
 import aboutImage from "../assets/about-image.svg";
 
 function About() {
--- src/pages/ServiceDetails.jsx
+++ src/pages/ServiceDetails.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 import serviceMainImage from "../assets/image (9).png";
 
